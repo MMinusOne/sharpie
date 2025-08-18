fa85d2bb85b6b5157cee458d33bce25c1766219d
MMinusOne <michael.business.lb@proton.me>
2025-08-18 15:24:49 +0300

    Type Converter

---
+++ Sharpie/TypeConverter.h
@@ -0,0 +1,17 @@
+#include "VariableTypes.h";
+#include <string>
+#include "TypeConverter.h";
+
+ VariableTypes convertToType(std::string type) {
+	if (type == "bool") {
+		return VariableTypes::Bool;
+	}
+	else if (type == "string") {
+		return VariableTypes::String;
+	}
+	else if (type == "int") {
+		return VariableTypes::Int;
+	}
+
+	return VariableTypes::Null;
+}
